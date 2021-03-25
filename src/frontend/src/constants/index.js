5589bffda5dc2b0b75cdb505c7486790f13dc0ad
Daniel Prohaska <dwp7kp@virginia.edu>
2021-03-24 22:41:01 -0400

    Frontend URL changes

--- src/frontend/src/constants/index.js
+++ src/frontend/src/constants/index.js
@@ -1,13 +1,13 @@
 // Production Settings. 
 // Note: When the frontend container is built for production, apply_deployment_vars.sh will update the PRODUCTION_API and set IN_PROD to true
 // Note: Do not change PROD_API or IN_PROD without making sure that apply_deployment_vars.sh doesn't break
-const PRODUCTION_API = 'https://student-performance-tracker.xyz/api';
-const IN_PROD = false;
+const PRODUCTION_API = 'http://hotcake.cs.virginia.edu:8000/api';
+const IN_PROD = true;
 
 // Constants
 export const toastTypes = ['success', 'error', 'info']; // info is default
 export const DEBUG_TOKEN = '12345';
-const DEVELOP_API = 'http://hotcake.cs.virginia.edu:8000/api';
+const DEVELOP_API = 'http://localhost:8000/api';
 
 // API URL
 export const API_URL = IN_PROD ? PRODUCTION_API : DEVELOP_API;
